a83b968766529b948baf431b3d900ced486c1e3e
mohamedhamdy2017 <34789872+mohamedhamdy2017@users.noreply.github.com>
2022-07-15 17:30:22 +0200

    add images as dummy data for slider

---
+++ SliderData.ts
@@ -0,0 +1,26 @@
+export interface Slide {
+  image: string;
+}
+
+export const sliderData: Slide[] = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1657543965558-16c3bd82ae14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1657624394328-a2ec199a5edf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1631812801948-2ade0adc7f2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1657643682449-2d911b844fcb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1657630623748-05f862d0c39e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+  },
+];
